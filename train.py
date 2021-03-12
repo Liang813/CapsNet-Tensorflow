b31dd62d1c678e7dc1a4e7ae5489b759b6fdd361
Liang813 <62755766+Liang813@users.noreply.github.com>
2021-03-12 15:30:10 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,16 +6,21 @@ from capsNet import CapsNet
 
 
 if __name__ == "__main__":
-    trainX, trainY, testX, testY = load_mnist(cfg.dataset)
+    
+    try:
+        trainX, trainY, testX, testY = load_mnist(cfg.dataset)
 
-    capsNet = CapsNet(is_training=cfg.is_training)
-    tf.logging.info('Graph loaded')
-    sv = tf.train.Supervisor()
+        capsNet = CapsNet(is_training=cfg.is_training)
+        tf.logging.info('Graph loaded')
+        sv = tf.train.Supervisor()
 
-    with sv.managed_session() as sess:
-        for epoch in range(cfg.epoch):
-            if sv.should_stop():
-                break
-            sess.run(capsNet.train_op)
+        with sv.managed_session() as sess:
+            for epoch in range(cfg.epoch):
+                if sv.should_stop():
+                    break
+                sess.run(capsNet.train_op)
 
-    tf.logging.info('Training done')
+        tf.logging.info('Training done')
+    except Exception as e:
+        print("ValueError")
+        print(e)
